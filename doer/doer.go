3dcc8d1d4e8e80a252e5f7b6927a590b30b0546a
evanson <evansonmwangi83@gmail.com>
2022-07-14 12:55:00 +0300

    auto generate mocks using go:generate

--- doer/doer.go
+++ doer/doer.go
@@ -1,5 +1,6 @@
 package doer
 
+//go:generate go run github.com/golang/mock/mockgen -destination mocks/mock_doer.go -package mocks github.com/basebandit/mocking/doer Doer
 type Doer interface {
 	DoSomething(int, string) error
 }
