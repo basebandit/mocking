0b5136a95e361ac49e9ed8450d97e3b226577385
evanson <evansonmwangi83@gmail.com>
2022-07-14 12:31:42 +0300

    test for returned error value

--- user/user_test.go
+++ user/user_test.go
@@ -1,6 +1,7 @@
 package user_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/basebandit/mocking/user"
@@ -20,4 +21,23 @@ func TestUse(t *testing.T) {
 	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(nil).Times(1)
 
 	testUser.Use()
+}
+
+func TestUseReturnsErrorFromDo(t *testing.T) {
+	mockCtlr := gomock.NewController(t)
+	defer mockCtlr.Finish()
+
+	dummyError := errors.New("dummy")
+	mockDoer := mocks.NewMockDoer(mockCtlr)
+	testUser := &user.User{Doer: mockDoer}
+
+	//Expect Do to be called once with 123 and "Hello GoMock" a parameters and return dummyError from the mocked call.
+	mockDoer.EXPECT().DoSomething(123, "Hello GoMock").Return(dummyError).Times(1)
+
+	err := testUser.Use()
+
+	if err != dummyError{
+		
+	}
+
 }
